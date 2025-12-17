25067e30ed7fbf07be53c20cfc8a803f98ee17a2
Hamza <hamza_k11@yahoo.com>
2025-12-18 02:37:15 +0500

    added upload picture functionality

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
 
 // routes
 app.use('/api/auth', require('./routes/auth'));
